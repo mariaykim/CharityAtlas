2451ad5224b102eb44a953d4fe9684ac4ba0237b
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2023-10-17 14:10:33 -0400

    Remove storyboard

---
+++ CharityAtlas/CharityAtlas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CharityAtlas
+//
+//  Created by Maria Kim on 10/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
